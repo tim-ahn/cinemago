82f1eeebe3a81bc952c101281b11e5e2b4da0676
david-diep <ddkotan@gmail.com>
2020-08-04 17:17:16 -0700

    navbar icons will now change view and color

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -7,12 +7,13 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: 'search',
+      view: 'home',
       results: [],
       trending: []
     };
     this.searchResults = this.searchResults.bind(this);
     this.getTrending = this.getTrending.bind(this);
+    this.changeView = this.changeView.bind(this);
   }
 
   searchResults(query) {
@@ -43,15 +44,21 @@ export default class App extends React.Component {
       });
   }
 
-  changeView() {
-
+  changeView(newPage) {
+    this.setState({ view: newPage });
   }
 
   render() {
+    let pageView;
+    if (this.state.view === 'home') {
+      pageView = <HomePage getTrending={this.getTrending} results={this.state.trending} />;
+    } else if (this.state.view === 'search') {
+      pageView = <HomeSearch searchResults={this.searchResults} results={this.state.results} />;
+    }
     return <>
-      {/* <HomeSearch searchResults={this.searchResults} results={this.state.results} /> */}
-      <HomePage getTrending={this.getTrending} results={this.state.trending} />
-      <Navbar/>
+      {pageView}
+
+      <Navbar changeView={this.changeView}/>
     </>;
   }
 }
