f4a95d3ddf19a1d0c7a4c6bc2c5848bca1b9ae9e
brandonktran <brandontran@berkeley.edu>
2020-08-07 19:05:26 +0000

    user can search for users front end- no functionality yet

--- client/components/home-search.jsx
+++ client/components/home-search.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import MovieLongCard from './movie-longcard';
-import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap';
+import classnames from 'classnames';
 
 class HomeSearch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: '', sortBy: 'popularity', dropdownOpen: false, dropDownShow: false };
+    this.state = { text: '', sortBy: 'popularity', dropdownOpen: false, dropDownShow: false, tab: '1', userText: '' };
     this.handleText = this.handleText.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.toggleDropDown = this.toggleDropDown.bind(this);
@@ -18,8 +19,19 @@ class HomeSearch extends React.Component {
     this.setState({ dropDownShow: true });
   }
 
+  handleSubmit2(event) {
+    event.preventDefault();
+    this.props.searchUsers(this.state.userText);
+  }
+
   handleText(event) {
+    console.log(event.target.value);
     this.setState({ text: event.target.value });
+  }
+
+  handleText2(event) {
+    console.log(event.target.value);
+    // this.setState({ userText: event.target.value });
   }
 
   toggleDropDown() {
@@ -31,6 +43,10 @@ class HomeSearch extends React.Component {
 
   }
 
+  switchTab(tab) {
+    if (this.state.tab !== tab) this.setState({ tab: tab });
+  }
+
   render() {
     let show = 'none';
     if (this.state.dropDownShow) {
@@ -38,33 +54,65 @@ class HomeSearch extends React.Component {
     }
 
     return (<>
-
-      <form className="container">
-        <h2 >Search Page</h2>
-        <input onChange={this.handleText} value={this.state.text} className="form-control" placeholder="Search for Movies" id="name-line"></input>
-
-        <button className="btn btn-primary" onClick={this.handleSubmit}>Search</button>
-
-      </form>
-
       <div className="container">
-        <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropDown} style={{ float: 'right', display: show }}>
-          <DropdownToggle caret>
-            Sort By
-          </DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem onClick={() => this.sortCategory()} id="popularity">Popularity</DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem onClick={() => this.sortCategory()} id="rating">Rating</DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem onClick={() => this.sortCategory()} id="date">Year</DropdownItem>
-          </DropdownMenu>
-        </ButtonDropdown>
-        <div className="row">
-          {this.props.results.map(item => (
-            <MovieLongCard key={item.id} id={item.id} poster_path={item.poster_path} fullInfo={item} addItemToList={this.props.addItemToList} lists={this.props.lists} />
-          ))}
-        </div>
+        <Nav tabs>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.tab === '1' })}
+              onClick={() => { this.switchTab('1'); }}
+            >
+              Movies
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.tab === '2' })}
+              onClick={() => { this.switchTab('2'); }}
+            >
+              Users
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={this.state.tab}>
+          <TabPane tabId="1">
+            <form className="container">
+              <h2 >Search Page</h2>
+              <input onChange={this.handleText} value={this.state.text} className="form-control" placeholder="Search for Movies" id="name-line"></input>
+
+              <button className="btn btn-primary" onClick={this.handleSubmit}>Search</button>
+
+            </form>
+
+            <div className="container">
+              <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropDown} style={{ float: 'right', display: show }}>
+                <DropdownToggle caret>
+                  Sort By
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem onClick={() => this.sortCategory()} id="popularity">Popularity</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem onClick={() => this.sortCategory()} id="rating">Rating</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem onClick={() => this.sortCategory()} id="date">Year</DropdownItem>
+                </DropdownMenu>
+              </ButtonDropdown>
+              <div className="row">
+                {this.props.results.map(item => (
+                  <MovieLongCard key={item.id} id={item.id} poster_path={item.poster_path} fullInfo={item} addItemToList={this.props.addItemToList} lists={this.props.lists} />
+                ))}
+              </div>
+            </div>
+          </TabPane>
+          <TabPane tabId="2">
+            <form className="container">
+              <h2 >Search Page</h2>
+              <input onChange={this.handleText2} value={this.state.userText} className="form-control" placeholder="Search for Users" id="user-line"></input>
+
+              <button className="btn btn-primary" onClick={this.handleSubmit2}>Search</button>
+
+            </form>
+          </TabPane>
+        </TabContent>
       </div>
 
     </>);
