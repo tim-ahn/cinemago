369a7852770bed19a62b4a71fcda69f644214bfb
brandonktran <brandontran@berkeley.edu>
2020-08-08 00:20:46 +0000

    fixed no messages error

--- client/components/user-messages.jsx
+++ client/components/user-messages.jsx
@@ -8,18 +8,26 @@ export default class UserMessages extends React.Component {
   }
 
   render() {
+    if (this.props.messages.length > 0) {
+      return (<>
+        <div className="container mt-2 justify-content-center">
+          <h1>My Messages</h1>
+          {this.props.messages.map((item, index) => {
+            return (<div key={index} className="border p-2 m-2 white">
+              <h5>From: {item.name}</h5>
+              <p>Content: {item.content}</p>
+            </div>);
+          })}
+        </div>
+      </>);
+    } else {
+      return (<>
+        <div className="container mt-2 justify-content-center">
+          <h1>My Messages</h1>
+          <p>No messages</p>
+        </div>
+      </>);
+    }
 
-    return (<>
-      <div className="container mt-2 justify-content-center">
-        <h1>My Messages</h1>
-        {this.props.messages.map((item, index) => {
-          return (<div key={index} className="border p-2 m-2 white">
-            <h5>From: {item.name}</h5>
-            <p>Content: {item.content}</p>
-          </div>);
-        })}
-      </div>
-
-    </>);
   }
 }
